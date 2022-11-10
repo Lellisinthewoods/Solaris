db6690ae044864380070342b2981999ff807c82c
Lellisinthewoods <ellinor.romin@hotmail.com>
2022-11-10 13:38:28 +0100

    buttons work. uglily

--- script.js
+++ script.js
@@ -1,42 +1,35 @@
 const BASE_URL = 'https://fathomless-shelf-54969.herokuapp.com';
-const API_KEY = `solaris-vKkkQHqQboi7c6JF`;
+const API_KEY =`solaris-vKkkQHqQboi7c6JF`; //HÅRDKODAD NYCKEL
 let planets_list = [];
-let buttons = document.querySelectorAll(`button`);
-for (i of buttons){
-    //debugger;
-    i.addEventListener(`click`, function(){
-        for(i=0;i<buttons.length;i++){
-            for (x of buttons){
-                if(i==x.value)
-                console.log(x.value)
-            }
-        }
-    })
-}
+let button0 = document.getElementById(`0`);
+let button1 = document.getElementById(`1`);
+let button2 = document.getElementById(`2`);
+let button3 = document.getElementById(`3`);
+let button4 = document.getElementById(`4`);
+let button5 = document.getElementById(`5`);
+let button6 = document.getElementById(`6`);
+let button7 = document.getElementById(`7`);
+let button8 = document.getElementById(`8`);
 
-async function getKey() 
+/*async function getKey() 
 {
     const response = await fetch(`${BASE_URL}/keys`, { method: 'POST' });
     const key_data = await response.json();
-    console.log(key_data);
-}
+    API_KEY = key_data;
+}*/
 
 async function getPlanets(number) 
 {
-    getKey();
-    const response = await fetch(`${BASE_URL}/bodies`, {
-        method: 'GET',
-        headers: 
-        {
-            'x-zocom': `${API_KEY}`
-        }
-    });
+    //getKey();
+    let response = await fetch("https://fathomless-shelf-54969.herokuapp.com/bodies", {
+        method: "GET",
+        headers: { "x-zocom": `${API_KEY}`},
+      });
     planets_list = await response.json();
-    console.log(planets_list);
     console.log(planets_list.bodies[number].name)
 }
 
-async function displayPlanets(planets)
+async function displayPlanets(planets) //behövs denna?
 {
     for (const planet of planets)
     {
@@ -44,28 +37,38 @@ async function displayPlanets(planets)
     }
 }
 
-//getKey();
-//getPlanets();
+button0.addEventListener(`click`, function() {
+    getPlanets(0);
+})
 
+button1.addEventListener(`click`, function() {
+    getPlanets(1);
+})
 
+button2.addEventListener(`click`, function() {
+    getPlanets(2);
+})
 
-function addClickEvent() { 
-    const planetElems = document.querySelectorAll('button'); // Hämtar alla button-element
-    console.log(planetElems);
-    for(const planetElem of planetElems) { // Loopar igenom alla button-element
-        planetElem.addEventListener('click', () => { // Sätter en addEventListener på varje button-element
-            const planetName = planetElem.getAttribute('name'); // Hämtar URL:en från src-attributet på img-taggen ''src'
-            console.log(planetName);
-            const fullPlanetElem = document.querySelector('button'); // Hämtar img-taggen med id full-image
-            fullPlanetElem.button = planetName;
-        });
-    }
-}
+button3.addEventListener(`click`, function() {
+    getPlanets(3);
+})
 
-/*button.addEventListener(`click`, async function() {
-    //addClickEvent();
-    console.log(button.value)
-    let number = button.value;
-    getPlanets(number);
-    //displayPlanets();
-})*/
+button4.addEventListener(`click`, function() {
+    getPlanets(4);
+})
+
+button5.addEventListener(`click`, function() {
+    getPlanets(5);
+})
+
+button6.addEventListener(`click`, function() {
+    getPlanets(6);
+})
+
+button7.addEventListener(`click`, function() {
+    getPlanets(7);
+})
+
+button8.addEventListener(`click`, function() {
+    getPlanets(8);
+})
